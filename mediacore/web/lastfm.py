6902d6b76aaf1e602488e92ca004e339a7efeaf7
Jerome Clerc <jererc@gmail.com>
2012-09-30 18:40:22 +0200

    handle all lastfm albums pages

--- mediacore/web/lastfm.py
+++ mediacore/web/lastfm.py
@@ -9,6 +9,7 @@ from mediacore.util.title import Title, clean
 
 
 MIN_ALBUM_TRACKS = 4
+MAX_ALBUMS_PAGES = 10
 MAX_SIMILAR_PAGES = 10
 RE_ARTISTS = re.compile(r'more artists', re.I)
 RE_ALBUMS = re.compile(r'top albums', re.I)
@@ -53,20 +54,57 @@ class Lastfm(Base):
                 if re_name.search(artist):
                     return urljoin(self.url, self._clean_url(links[0].get('href')))
 
-    def _get_albums_url(self, query):
-        url = self._get_artist_url(query)
-        if not url:
-            return
-        data = self.browser.get_unicode_data(url=url)
-        if not data:
-            return
+    def _artist_albums(self, url):
+        for i in range(MAX_ALBUMS_PAGES):
+            data = self.browser.get_unicode_data(url=url)
+            if not data:
+                return
 
-        tree = html.fromstring(data)
-        links = tree.cssselect('.artist-top-albums a')
-        if not links:
-            logger.debug('failed to find artist albums link for "%s" at %s', query, url)
-        elif RE_ALBUMS.search(links[0].text):
-            return urljoin(self.url, links[0].get('href'))
+            tree = html.fromstring(data)
+            for tag in tree.cssselect('.album-item'):
+                log = html.tostring(tag, pretty_print=True)
+
+                meta_tags = tag.cssselect('[itemprop="name"]')
+                if not meta_tags:
+                    continue
+
+                name = clean(meta_tags[0].get('content', ''), 1)
+                if not name:
+                    continue
+                info_album = {'name': name}
+
+                url_tags = tag.cssselect('a')
+                if not url_tags:
+                    logger.error('failed to get album url from %s', log)
+                else:
+                    info_album['url'] = urljoin(self.url, url_tags[0].get('href'))
+
+                date_tags = tag.cssselect('time')
+                if not date_tags:
+                    continue
+                try:
+                    date = RE_DATE_ALBUM.search(date_tags[0].get('datetime'))
+                    info_album['date'] = int(date.group(1))
+                except Exception:
+                    continue
+
+                # Check nb tracks
+                tracks_tags = tag.cssselect('[itemprop="numTracks"]')
+                if not tracks_tags:
+                    continue
+                try:
+                    nb_tracks = int(tracks_tags[0].text)
+                except ValueError:
+                    continue
+                if nb_tracks < MIN_ALBUM_TRACKS:
+                    continue
+
+                yield info_album
+
+            next_link = tree.cssselect('.pagination .nextlink')
+            if not next_link:
+                return
+            url = urljoin(self.url, next_link[0].get('href'))
 
     def _get_info(self, query):
         url = self._get_artist_url(query)
@@ -90,60 +128,15 @@ class Lastfm(Base):
         # Get albums
         links = tree.cssselect('.artist-top-albums a')
         if not links:
-            logger.debug('failed to find artist albums link for "%s" at %s', query, url)
+            logger.debug('failed to find albums link for "%s" at %s', query, url)
             return info
         elif not RE_ALBUMS.search(links[0].text):
             return
 
         url_albums = urljoin(self.url, links[0].get('href'))
-        data = self.browser.get_unicode_data(url=url_albums)
-        if not data:
-            return
-
-        tree = html.fromstring(data)
-        for tag in tree.cssselect('.albums li .album-item-detail-wrapper'):
-            log = html.tostring(tag, pretty_print=True)
-
-            info_album = {}
-            if info.get('genre'):
-                info_album['genre'] = info['genre']
-
-            try:
-                info_album['name'] = clean(html.tostring(tag[0][0]), 1)
-            except Exception:
-                logger.error('failed to get album name from %s', log)
-                continue
-            if not info_album['name']:
-                continue
-            try:
-                info_album['url'] = urljoin(self.url, tag[0][0].get('href'))
-            except Exception:
-                logger.error('failed to get album url from %s', log)
-
-            date_tags = tag.cssselect('time')
-            if not date_tags:
-                continue
-            try:
-                date = RE_DATE_ALBUM.search(date_tags[0].get('datetime'))
-                info_album['date'] = int(date.group(1))
-            except Exception:
-                continue
-
-            # Check nb tracks
-            tracks_tags = tag.cssselect('[itemprop="numTracks"]')
-            if not tracks_tags:
-                continue
-            try:
-                nb_tracks = int(tracks_tags[0].text)
-            except ValueError:
-                continue
-            if nb_tracks < MIN_ALBUM_TRACKS:
-                continue
-
+        for info_album in self._artist_albums(url_albums):
+            info_album['genre'] = info['genre']
             info['albums'].append(info_album)
-
-        if not info['albums']:
-            logger.debug('failed to get albums from "%s"', query)
 
         return info
 
@@ -165,10 +158,10 @@ class Lastfm(Base):
 
         tree = html.fromstring(data)
         links = tree.cssselect('.similar-artists a')
-        if not links:
-            logger.error('failed to find similar artists link for %s at %s', query, url)
-        elif RE_SIMILAR.search(links[0].text):
+        if links and RE_SIMILAR.search(links[0].text):
             return urljoin(self.url, links[0].get('href'))
+
+        logger.error('failed to find similar artists link for %s at %s', query, url)
 
     def _similar_artists(self, url):
         for i in range(MAX_SIMILAR_PAGES):
