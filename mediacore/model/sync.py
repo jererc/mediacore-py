dc1c10e850b33ca7ec445b5f269367687d65ac55
Jerome Clerc <jererc@gmail.com>
2012-12-27 21:06:05 +0100

    clean sync

--- mediacore/model/sync.py
+++ mediacore/model/sync.py
@@ -7,14 +7,13 @@ class Sync(Model):
     COL = 'syncs'
 
     @classmethod
-    def add(cls, user, dst, media_id=None, parameters=None):
+    def add(cls, user, category, dst='', parameters=None):
         doc = {
             'user': user,
+            'category': category,
             'dst': dst,
             'parameters': parameters or {},
             }
-        if media_id:
-            doc['media_id'] = media_id
         if not cls.find_one(doc):
             doc['created'] = datetime.utcnow()
             return cls.insert(doc, safe=True)
