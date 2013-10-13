c05816ca5fbe25563b3bd628af09e565ee0918fb
Jerome Clerc <jererc@gmail.com>
2013-10-13 12:24:47 +0200

    update google

--- mediacore/web/google.py
+++ mediacore/web/google.py
@@ -1,5 +1,5 @@
 import re
-from urlparse import urlparse
+from urlparse import urlparse, urljoin
 import logging
 
 from lxml import html
@@ -17,11 +17,17 @@ logger = logging.getLogger(__name__)
 
 class Google(Base):
     URL = 'http://www.google.com'
+    ROBUST_FACTORY = True
 
     def _next(self, page):
-        return self.browser.follow_link(
-                text_regex=re.compile(r'^\D*%s\D*$' % page),
-                url_regex=RE_URL_SEARCH)
+        for link in self.browser.cssselect('#nav a'):
+            try:
+                page_ = int(clean(self.get_link_text(html.tostring(link))))
+            except ValueError:
+                continue
+            if page_ == page:
+                url = urljoin(self.url, link.get('href'))
+                return self.browser.open(url)
 
     def results(self, query, pages_max=1):
         for page in range(1, pages_max + 1):
@@ -41,17 +47,16 @@ class Google(Base):
                 url = links[0].get('href')
                 if not url or not urlparse(url).scheme:
                     continue
-                title = self.get_link_text(html.tostring(links[0]))
+                title = clean(self.get_link_text(html.tostring(links[0])))
                 if not title:
                     continue
-                result = {
-                    'title': clean(title),
+                yield {
+                    'title': title,
                     'url': url,
                     'page': page,
                     }
-                yield result
 
-    def get_nb_results(self, query):
+    def get_results_count(self, query):
         '''Get the results count for a query.
         '''
         self.browser.submit_form(self.url, fields={'q': query})
@@ -65,7 +70,7 @@ class Google(Base):
     def get_most_popular(self, queries):
         '''Get the most popular query from a list of queries.
         '''
-        stat = [(self.get_nb_results(q), q) for q in queries]
+        stat = [(self.get_results_count(q), q) for q in queries]
         res, query = sorted(stat)[-1]
         if res:
             return query
